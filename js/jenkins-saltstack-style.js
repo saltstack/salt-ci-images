7ae462a8307736a1923e2fd436ffd1a2ed9ad361
Pedro Algarvio <pedro@algarvio.me>
2013-09-11 17:01:45 +0100

    Use Yahoo's onDomReady

--- js/jenkins-saltstack-style.js
+++ js/jenkins-saltstack-style.js
@@ -1,5 +1,5 @@
 // Jenkins js tweaks
-setTimeout(function(){
+YAHOO.util.Event.onDOMReady(function() {
   document.getElementById('search-box').setAttribute("type","search");
   document.getElementById('main-table').setAttribute('style', 'none');
-}, 500);
+});

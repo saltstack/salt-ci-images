47bb3b0b6f47f77499c9e00732c10452ced277a2
Pedro Algarvio <pedro@algarvio.me>
2013-09-11 16:32:55 +0100

    Switch background logo.

--- js/jenkins-saltstack-style.js
+++ js/jenkins-saltstack-style.js
@@ -1,4 +1,5 @@
 // Jenkins js tweaks
 setTimeout(function(){
   document.getElementById('search-box').setAttribute("type","search");
+  document.getElementById('main-table').removeAttribute('style');
 }, 350);

cd2c95099b698535529d694f94d8ed461a8c6134
Pedro Algarvio <pedro@algarvio.me>
2013-12-09 19:50:29 +0000

    Add a module to stream the runtests output.

---
+++ _modules/runtests.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+'''
+    :codeauthor: :email:`Pedro Algarvio (pedro@algarvio.me)`
+    :copyright: © 2013 by the UfSoft.org Team, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+
+
+    runtests.py
+    ~~~~~~~~~~~
+
+    Simple module to stream the tests suite output since the current salt cmd
+    module is unable.
+'''
+
+import os
+import time
+import salt.utils.vt
+
+
+def run(cmd, env=None):
+
+    if not env:
+        env = {}
+
+    run_env = os.environ.copy()
+    run_env.update(env)
+
+    terminal = vt.Terminal(
+        cmd,
+        env=run_env,
+        stream_stdout=True,
+        stream_stderr=True,
+        log_stdout=True,
+        log_stderr=True
+    )
+
+    while True:
+        terminal.recv()
+
+        if exiting:
+            break
+
+        if not terminal.isalive:
+            exiting = True
+
+        time.sleep(0.025)
+
+    return terminal.exitstatus

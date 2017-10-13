c9ce23e69973e92ebdb8be15a57ceeef9269db8d
Daniel Wallace <danielwallace@gtmanfred.com>
2017-10-12 18:55:03 -0600

    add kitchen binary to 2016.11

---
+++ kitchen/kitchen.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env {{ 'python3' if salt.pillar.get('py3') else 'python2' }}
+import salt.client
+import salt.config
+
+__opts__ = salt.config.minion_config('/tmp/kitchen/etc/salt/minion')
+__opts__['file_client'] = 'local'
+caller = salt.client.Caller(mopts=__opts__)
+if caller.cmd('config.get', 'py3', False):
+    print('python3')
+elif caller.cmd('config.get', 'os_family') == 'RedHat' and int(caller.cmd('config.get', 'osmajorrelease')) == 6:
+    print('python2.7')
+else:
+    print('python2')

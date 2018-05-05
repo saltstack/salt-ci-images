4235d5241faf8af36c6b95886016424d7224b3f3
Daniel Wallace <danielwallace@gtmanfred.com>
2018-05-04 23:00:46 -0500

    put all pip version stuff in one place

--- _states/pip2_state.py
+++ _states/pip2_state.py
@@ -40,41 +40,6 @@ for name in dir(salt.states.pip_state):
         globals()[name] = namespaced_function(attr, globals())
 
 
-def _get_pip_bin(bin_env):
-    '''
-    Locate the pip binary, either from `bin_env` as a virtualenv, as the
-    executable itself, or from searching conventional filesystem locations
-    '''
-    pip_bin_name = 'pip2'
-    if not bin_env:
-        which_result = __salt__['cmd.which_bin']([pip_bin_name])
-        if salt.utils.is_windows() and six.PY2:
-            which_result.encode('string-escape')
-        if which_result is None:
-            raise CommandNotFoundError('Could not find a `pip` binary')
-        return which_result
-
-    # try to get pip bin from virtualenv, bin_env
-    if os.path.isdir(bin_env):
-        if salt.utils.is_windows():
-            if six.PY2:
-                pip_bin = os.path.join(
-                    bin_env, 'Scripts', 'pip.exe').encode('string-escape')
-            else:
-                pip_bin = os.path.join(bin_env, 'Scripts', 'pip.exe')
-        else:
-            pip_bin = os.path.join(bin_env, 'bin', pip_bin_name)
-        if os.path.isfile(pip_bin):
-            return pip_bin
-        msg = 'Could not find a `pip` binary in virtualenv {0}'.format(bin_env)
-        raise CommandNotFoundError(msg)
-    # bin_env is the pip binary
-    elif os.access(bin_env, os.X_OK):
-        if os.path.isfile(bin_env) or os.path.islink(bin_env):
-            return bin_env
-    else:
-        raise CommandNotFoundError('Could not find a `pip` binary')
-
 __virtualname__ = 'pip2'
 
 
@@ -92,7 +57,11 @@ def installed(name, **kwargs):
     if extra_index_url is None:
         extra_index_url = 'https://pypi.python.org/simple'
 
-    bin_env = _get_pip_bin(kwargs.get('bin_env'))
+    if __grains__['os_family'] == 'RedHat' and int(__grains__['osmajorrelease']) == 6:
+        pip_bin_name = 'pip2.7'
+    else:
+        pip_bin_name = 'pip2'
+    bin_env = __salt__['pip.get_pip_bin'](kwargs.get('bin_env'), pip_bin_name)
     if isinstance(bin_env, list):
         bin_env = bin_env[0]
     log.warning('pip2 binary found: %s', bin_env)

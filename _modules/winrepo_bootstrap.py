defe0a1d5163d3b25c6198277720273e9cd3f67d
Pedro Algarvio <pedro@algarvio.me>
2019-02-26 02:00:12 +0000

    Fix winrepo bootstrap under Py3

--- _modules/winrepo_bootstrap.py
+++ _modules/winrepo_bootstrap.py
@@ -5,6 +5,7 @@ try:
 except (ImportError, AttributeError):
     from salt.utils import is_windows
 from salt.exceptions import CommandExecutionError
+import salt.ext.six as six
 
 log = logging.getLogger(__name__)
 __all__ = [
@@ -43,33 +44,37 @@ def download_git_repos():
     ret = {}
     for remotes, base_dir in winrepo_cfg:
         for remote_info in remotes:
-            if '/' in remote_info:
-                targetname = remote_info.split('/')[-1]
-            else:
-                targetname = remote_info
-            rev = 'HEAD'
             try:
-                rev, remote_url = remote_info.strip().split()
-            except ValueError:
-                remote_url = remote_info
-            gittarget = os.path.join(base_dir, targetname).replace('.', '_')
-            client, path = dulwich.client.get_transport_and_path(remote_url)
-            if os.path.exists(gittarget):
-                local_repo = dulwich.repo.Repo(gittarget)
-            else:
-                os.makedirs(gittarget)
-                local_repo = dulwich.repo.Repo.init(gittarget)
-            remote_refs = client.fetch(remote_url, local_repo)
-            if rev not in remote_refs:
-                raise CommandExecutionError(
-                    'Failed to find remote ref: {0}'.format(rev)
+                if '/' in remote_info:
+                    targetname = remote_info.split('/')[-1]
+                else:
+                    targetname = remote_info
+                rev = 'HEAD'
+                try:
+                    rev, remote_url = remote_info.strip().split()
+                except ValueError:
+                    remote_url = remote_info
+                gittarget = os.path.join(base_dir, targetname).replace('.', '_')
+                client, path = dulwich.client.get_transport_and_path(remote_url)
+                if os.path.exists(gittarget):
+                    local_repo = dulwich.repo.Repo(gittarget)
+                else:
+                    os.makedirs(gittarget)
+                    local_repo = dulwich.repo.Repo.init(gittarget)
+                remote_refs = client.fetch(remote_url, local_repo)
+                if six.b(rev) not in remote_refs.refs:
+                    raise CommandExecutionError(
+                        'Failed to find remote ref: {0}'.format(rev)
+                    )
+                local_repo[six.b(rev)] = remote_refs[six.b(rev)]
+                dulwich.index.build_index_from_tree(
+                    local_repo.path, local_repo.index_path(),
+                    local_repo.object_store, local_repo[six.b('head')].tree
                 )
-            local_repo[rev] = remote_refs[rev]
-            dulwich.index.build_index_from_tree(
-                local_repo.path, local_repo.index_path(),
-                local_repo.object_store, local_repo['head'].tree
-            )
-            ret.update({remote_info: True})
+                ret.update({remote_info: True})
+            except Exception as exc:
+                log.exception('Failed to process remote_info: %s', remote_info, exc_info=True)
+                raise
     if __salt__['winrepo.genrepo']():
         return ret
     return False

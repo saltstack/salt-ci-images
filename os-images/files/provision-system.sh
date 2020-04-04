14706d061972ce7f6a93f4297fdf674b162f56e3
Pedro Algarvio <pedro@algarvio.me>
2020-04-04 01:44:37 +0100

    Use the direct path to `salt-call`.
    We don't want to activate the pyenv since that would get `nox` installed
    into it, which, and the end we nuke it.

--- os-images/files/provision-system.sh
+++ os-images/files/provision-system.sh
@@ -3,16 +3,9 @@
 # Exit on failures
 set -e
 
-export PATH="~/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-
 echo "Contents of ${SALT_ROOT_DIR}:"
 ls -lah ${SALT_ROOT_DIR}
 
-# Select the instaled Python
-pyenv shell ${SALT_PY_VERSION}
-
 COMMAND="salt-call --config-dir=${SALT_ROOT_DIR}/conf --local --log-level=info --file-root=${SALT_ROOT_DIR}/states --pillar-root=${SALT_ROOT_DIR}/pillar state.sls ${SALT_STATE} --retcode-passthrough"
 echo "Running: ${COMMAND}"
-eval "${COMMAND}"
+eval "~/.pyenv/versions/${SALT_PY_VERSION}/bin/${COMMAND}"

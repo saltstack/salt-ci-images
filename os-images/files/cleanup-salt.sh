394c42cf13214bd4fd63347f0b08f34c400103c6
Pedro Algarvio <pedro@algarvio.me>
2020-04-10 11:10:43 +0100

    We no longer need to create a bogus salt-call binary

--- os-images/files/cleanup-salt.sh
+++ os-images/files/cleanup-salt.sh
@@ -8,9 +8,3 @@ set -x
 rm -rf ${SALT_ROOT_DIR}
 rm -rf ~/.pyenv
 rm -rf ~/.cache/pip
-
-# While we don't have kitchen-salt 0.6.1 out, fake an installed salt-call binary
-echo "Creating bogus salt-call binary until kitchen-salt 0.6.1 is out"
-echo "#!/bin/bash" > /bin/salt-call
-echo "echo \"salt-call ${SALT_VERSION}\"" >> /bin/salt-call
-chmod +x /bin/salt-call

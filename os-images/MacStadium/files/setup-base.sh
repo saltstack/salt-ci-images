3fe1ca28d955180272add74272887f48ad3426c4
Pedro Algarvio <pedro@algarvio.me>
2020-12-28 08:07:46 +0000

    Use the new homebrew installer

--- os-images/MacStadium/files/setup-base.sh
+++ os-images/MacStadium/files/setup-base.sh
@@ -28,4 +28,4 @@ if [ "$OSX_VERS" -eq 13 ]; then
 fi
 
 echo "====> Installing homebrew"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"

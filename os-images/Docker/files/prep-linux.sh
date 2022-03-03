adcd5322772cb15ef643c36d28f205336ebe3e11
Gareth J. Greenaway <gareth@saltstack.com>
2022-03-03 15:27:56 -0800

    moving the profile.d command out of the prep-linux.sh script which is run locally and into the packer files.

--- os-images/Docker/files/prep-linux.sh
+++ os-images/Docker/files/prep-linux.sh
@@ -5,10 +5,6 @@ set -e
 # Echo what runs
 set -x
 
-# This will be used later by the Salt tests to skip any tests
-# that shouldn't run in Docker containers.
-echo "export ON_DOCKER=1" > /etc/profile.d/on_docker.sh
-
 # Call the main prep file
 . os-images/files/prep-uploads.sh
 

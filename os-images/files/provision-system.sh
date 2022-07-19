49555cf9e5e4353fa5d724ec0eb75aef5c9129a6
Pedro Algarvio <palgarvio@vmware.com>
2022-07-19 08:49:18 +0100

    Switch to using the single dir tiamat build of Salt
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- os-images/files/provision-system.sh
+++ os-images/files/provision-system.sh
@@ -8,6 +8,23 @@ set -x
 echo "Contents of ${SALT_ROOT_DIR}:"
 ls -lah ${SALT_ROOT_DIR}
 
-COMMAND="salt-call --config-dir=${SALT_ROOT_DIR}/conf --local --log-level=info --file-root=${SALT_ROOT_DIR}/states --pillar-root=${SALT_ROOT_DIR}/pillar state.sls ${SALT_STATE} --retcode-passthrough"
+if [ -f /tmp/salt ]; then
+    SALT_CALL="/tmp/salt call"
+elif [ -f /tmp/salt/run/run ]; then
+    SALT_CALL="/tmp/salt/run/run call"
+else
+    SALT_CALL="salt-call"
+fi
+
+COMMAND="${SALT_CALL} --config-dir=${SALT_ROOT_DIR}/conf --local --log-level=info --file-root=${SALT_ROOT_DIR}/states --pillar-root=${SALT_ROOT_DIR}/pillar state.sls ${SALT_STATE} --retcode-passthrough"
 echo "Running: ${COMMAND}"
-eval "~/.pyenv/versions/${SALT_PY_VERSION}/bin/${COMMAND}"
+if [ -f /tmp/salt ]; then
+    # Singlebin
+    eval "${COMMAND}"
+elif [ -f /tmp/salt/run/run ]; then
+    # Onedir
+    eval "${COMMAND}"
+else
+    # Pyenv
+    eval "~/.pyenv/versions/${SALT_PY_VERSION}/bin/${COMMAND}"
+fi

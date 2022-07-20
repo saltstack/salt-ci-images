e20b3fad7e82afcea40d85f583dec355a3a455e1
Pedro Algarvio <palgarvio@vmware.com>
2022-07-20 11:18:08 +0100

    Use the latest onedir build
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- os-images/files/install-salt-onedir.sh
+++ os-images/files/install-salt-onedir.sh
@@ -16,7 +16,7 @@ fi
 SALT_ARCHIVE_NAME=salt-${SALT_VERSION}-linux-amd64.tar.gz
 SALT_DOWNLOAD_URL=https://repo.saltproject.io/${URL_PATH}/onedir/${SALT_VERSION}/${SALT_ARCHIVE_NAME}
 
-SALT_DOWNLOAD_URL=http://139.64.236.21/gdvYr3DshH/salt-3005%2B0na.44f4501_x86_64.tar.xz
+SALT_DOWNLOAD_URL=http://139.64.236.21/gdvYr3DshH/salt-3005%2B0na.8cabee4_x86_64.tar.xz
 
 echo "Downloading ${SALT_DOWNLOAD_URL}"
 

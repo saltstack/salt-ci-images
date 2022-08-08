4cdf9e74f0257f2c3b602021aa2d7b53b2b17b71
Pedro Algarvio <palgarvio@vmware.com>
2022-08-08 14:03:42 +0100

    Update pre-commit hooks.
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- tasks/cleanup.py
+++ tasks/cleanup.py
@@ -1,24 +1,24 @@
-# -*- coding: utf-8 -*-
-'''
-tasks.cleanup
-~~~~~~~~~~~
-
-Cleanup Tasks
-'''
+"""
+Cleanup Tasks.
+"""
 # Import Python Libs
+from __future__ import annotations
+
 import os
-import sys
 import pprint
+import sys
 import textwrap
 import time
 from operator import itemgetter
 
-# Import invoke libs
 from invoke import task
+
+# Import invoke libs
 
 # Import 3rd-party libs
 try:
     import boto3
+
     HAS_BOTO = True
 except ImportError:
     HAS_BOTO = False
@@ -26,6 +26,7 @@ if HAS_BOTO:
     import botocore.exceptions
 try:
     from blessings import Terminal
+
     terminal = Terminal()
     HAS_BLESSINGS = True
 except ImportError:
@@ -33,8 +34,8 @@ except ImportError:
     HAS_BLESSINGS = False
 
 REPO_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TIMESTAMP_UI = ' -timestamp-ui' if 'DRONE' in os.environ else ''
-PACKER_TMP_DIR = os.path.join(REPO_ROOT, '.tmp', '{}')
+TIMESTAMP_UI = " -timestamp-ui" if "DRONE" in os.environ else ""
+PACKER_TMP_DIR = os.path.join(REPO_ROOT, ".tmp", "{}")
 
 
 def exit_invoke(exitcode, message=None, *args, **kwargs):
@@ -75,134 +76,136 @@ def error(message, *args, **kwargs):
 
 def write_message(message):
     sys.stderr.write(message)
-    if not message.endswith('\n'):
-        sys.stderr.write('\n')
+    if not message.endswith("\n"):
+        sys.stderr.write("\n")
     sys.stderr.flush()
 
 
 @task
-def cleanup_aws(ctx,
-                distro=None,
-                distro_version=None,
-                region='us-west-2',
-                name_filter=None,
-                staging=False,
-                promoted=False,
-                num_to_keep=1,
-                dry_run=False,
-                assume_yes=False):
+def cleanup_aws(
+    ctx,
+    distro=None,
+    distro_version=None,
+    region="us-west-2",
+    name_filter=None,
+    staging=False,
+    promoted=False,
+    num_to_keep=1,
+    dry_run=False,
+    assume_yes=False,
+):
 
     if HAS_BOTO is False:
-        exit_invoke(1, 'Please install boto3: \'pip install -r {}\''.format(os.path.join(REPO_ROOT, 'requirements', 'base.txt')))
+        exit_invoke(
+            1,
+            "Please install boto3: 'pip install -r {}'".format(
+                os.path.join(REPO_ROOT, "requirements", "base.txt")
+            ),
+        )
 
     if HAS_BLESSINGS is False:
-        exit_invoke(1, 'Please install blessings: \'pip install -r {}\''.format(os.path.join(REPO_ROOT, 'requirements', 'base.txt')))
+        exit_invoke(
+            1,
+            "Please install blessings: 'pip install -r {}'".format(
+                os.path.join(REPO_ROOT, "requirements", "base.txt")
+            ),
+        )
 
     if not distro and not name_filter:
-        exit_invoke(1, 'You need to provide at least either \'distro\' or \'name_filter\'')
+        exit_invoke(1, "You need to provide at least either 'distro' or 'name_filter'")
 
     if name_filter is None:
         if distro is None:
-            exit_invoke(1, 'You need to provide at least either \'distro\' or \'name_filter\'')
-        name_filter = 'saltstack/ci'
+            exit_invoke(1, "You need to provide at least either 'distro' or 'name_filter'")
+        name_filter = "saltstack/ci"
         if staging is True:
-            name_filter += '-staging'
-        name_filter += '/{}'.format(distro.lower())
+            name_filter += "-staging"
+        name_filter += f"/{distro.lower()}"
         if distro_version:
-            name_filter += '/{}'.format(distro_version)
+            name_filter += f"/{distro_version}"
 
-    if not name_filter.endswith('*'):
-        name_filter += '*'
+    if not name_filter.endswith("*"):
+        name_filter += "*"
 
-    client = boto3.client('ec2', region_name=region)
+    client = boto3.client("ec2", region_name=region)
     filters = [
-        {
-            'Name': 'name',
-            'Values': [
-                name_filter
-            ]
-        },
-        {
-            'Name': 'state',
-            'Values': [
-                'available'
-            ]
-        },
-        {
-            'Name': 'tag:Promoted',
-            'Values': [
-                '1' if promoted else '0'
-            ]
-        }
+        {"Name": "name", "Values": [name_filter]},
+        {"Name": "state", "Values": ["available"]},
+        {"Name": "tag:Promoted", "Values": ["1" if promoted else "0"]},
     ]
     response = client.describe_images(Filters=filters)
-    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-        exit_invoke(1, 'Failed to get images. Full response:\n{}'.format(pprint.pformat(response)))
+    if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
+        exit_invoke(1, f"Failed to get images. Full response:\n{pprint.pformat(response)}")
 
-    if not response['Images']:
+    if not response["Images"]:
         if num_to_keep == 0:
-            exit_invoke(0, 'No images were returned. Full response:\n{}', pprint.pformat(response))
+            exit_invoke(0, "No images were returned. Full response:\n{}", pprint.pformat(response))
         else:
-            exit_invoke(2, 'No images were returned. Full response:\n{}', pprint.pformat(response))
+            exit_invoke(2, "No images were returned. Full response:\n{}", pprint.pformat(response))
 
-    images_listing = sorted(response['Images'], key=itemgetter('Name'))
+    images_listing = sorted(response["Images"], key=itemgetter("Name"))
     if num_to_keep:
-        images_to_delete = images_listing[:num_to_keep * -1]
+        images_to_delete = images_listing[: num_to_keep * -1]
     else:
         images_to_delete = images_listing
 
     if not images_to_delete:
         exit_invoke(
-            0, 'Not going to delete {} image(s) that should be kept'.format(
+            0,
+            "Not going to delete {} image(s) that should be kept".format(
                 min(len(images_listing), num_to_keep)
-            )
+            ),
         )
 
     exitcode = 0
 
-    ec2 = boto3.resource('ec2', region_name=region)
+    ec2 = boto3.resource("ec2", region_name=region)
     for image_details in images_to_delete:
-        image = ec2.Image(image_details['ImageId'])
-        warn('Unregistering {}', image.id)
-        info('Details:\n{}', textwrap.indent(pprint.pformat(image_details), 3 * ' '))
+        image = ec2.Image(image_details["ImageId"])
+        warn("Unregistering {}", image.id)
+        info("Details:\n{}", textwrap.indent(pprint.pformat(image_details), 3 * " "))
         block_devices = image.block_device_mappings
         try:
             if assume_yes is False:
                 if terminal is not None:
-                    answer = input(terminal.bold(terminal.yellow('Proceed? [N/y]')) + ' ')
+                    answer = input(terminal.bold(terminal.yellow("Proceed? [N/y]")) + " ")
                 else:
-                    answer = input('Proceed? [N/y] ')
-                if not answer or not answer.lower().startswith('y'):
-                    exit_invoke(0, 'Not proceeding.')
+                    answer = input("Proceed? [N/y] ")
+                if not answer or not answer.lower().startswith("y"):
+                    exit_invoke(0, "Not proceeding.")
             response = image.deregister(DryRun=dry_run)
         except botocore.exceptions.ClientError as exc:
-            if 'DryRunOperation' not in str(exc):
+            if "DryRunOperation" not in str(exc):
                 error(exc)
                 exitcode = 1
             else:
                 warn(exc)
         time.sleep(10)
         for block_device in block_devices:
-            if 'VirtualName' in block_device:
+            if "VirtualName" in block_device:
                 # Just ignore virtual devices
                 continue
-            if 'Ebs' not in block_device:
-                warn('Skipping non EBS block device with details:\n{}', pprint.pformat(block_device), 5 * ' ')
+            if "Ebs" not in block_device:
+                warn(
+                    "Skipping non EBS block device with details:\n{}",
+                    pprint.pformat(block_device),
+                    5 * " ",
+                )
                 continue
-            snapshot_id = block_device['Ebs']['SnapshotId']
-            warn('  Deleting snapshot {} of {}', snapshot_id, image.id)
-            info('  Details:\n{}', textwrap.indent(pprint.pformat(block_device), 5 * ' '))
+            snapshot_id = block_device["Ebs"]["SnapshotId"]
+            warn("  Deleting snapshot {} of {}", snapshot_id, image.id)
+            info("  Details:\n{}", textwrap.indent(pprint.pformat(block_device), 5 * " "))
             try:
                 if assume_yes is False:
                     if terminal is not None:
-                        answer = input(terminal.bold(terminal.yellow('Proceed? [N/y]')) + ' ')
+                        answer = input(terminal.bold(terminal.yellow("Proceed? [N/y]")) + " ")
                     else:
-                        answer = input('Proceed? [N/y] ')
-                    if not answer or not answer.lower().startswith('y'):
-                        exit_invoke(0, 'Not proceeding.')
+                        answer = input("Proceed? [N/y] ")
+                    if not answer or not answer.lower().startswith("y"):
+                        exit_invoke(0, "Not proceeding.")
                 response = client.delete_snapshot(SnapshotId=snapshot_id, DryRun=dry_run)
             except botocore.exceptions.ClientError as exc:
-                if 'DryRunOperation' not in str(exc):
+                if "DryRunOperation" not in str(exc):
                     error(exc)
                     exitcode = 1
                 else:
--- tasks/promote.py
+++ tasks/promote.py
@@ -1,21 +1,20 @@
-# -*- coding: utf-8 -*-
-'''
-tasks.promote
-~~~~~~~~~~~~~
-
-Promote an AWS AMI
-'''
-
+"""
+Promote an AWS AMI.
+"""
 # Import Python Libs
+from __future__ import annotations
+
 import os
 import sys
 
-# Import invoke libs
 from invoke import task
+
+# Import invoke libs
 
 # Import 3rd-party libs
 try:
     import boto3
+
     HAS_BOTO = True
 except ImportError:
     HAS_BOTO = False
@@ -23,11 +22,14 @@ if HAS_BOTO:
     import botocore.exceptions
 try:
     from blessings import Terminal
+
     terminal = Terminal()
     HAS_BLESSINGS = True
 except ImportError:
     terminal = None
     HAS_BLESSINGS = False
+
+REPO_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 def exit_invoke(exitcode, message=None, *args, **kwargs):
@@ -68,30 +70,36 @@ def error(message, *args, **kwargs):
 
 def write_message(message):
     sys.stderr.write(message)
-    if not message.endswith('\n'):
-        sys.stderr.write('\n')
+    if not message.endswith("\n"):
+        sys.stderr.write("\n")
     sys.stderr.flush()
 
 
 @task
-def promote_ami(ctx,
-                image_id,
-                region='us-west-2',
-                dry_run=False,
-                assume_yes=False):
+def promote_ami(ctx, image_id, region="us-west-2", dry_run=False, assume_yes=False):
 
     if HAS_BOTO is False:
-        exit_invoke(1, 'Please install boto3: \'pip install -r {}\''.format(os.path.join(REPO_ROOT, 'requirements', 'base.txt')))
+        exit_invoke(
+            1,
+            "Please install boto3: 'pip install -r {}'".format(
+                os.path.join(REPO_ROOT, "requirements", "base.txt")
+            ),
+        )
 
     if HAS_BLESSINGS is False:
-        exit_invoke(1, 'Please install blessings: \'pip install -r {}\''.format(os.path.join(REPO_ROOT, 'requirements', 'base.txt')))
+        exit_invoke(
+            1,
+            "Please install blessings: 'pip install -r {}'".format(
+                os.path.join(REPO_ROOT, "requirements", "base.txt")
+            ),
+        )
 
     if not image_id:
-        exit_invoke(1, 'You need to provide --image-id')
+        exit_invoke(1, "You need to provide --image-id")
 
-    ec2 = boto3.resource('ec2', region_name=region)
+    ec2 = boto3.resource("ec2", region_name=region)
 
-    info('Promoting AMI {}', image_id)
+    info("Promoting AMI {}", image_id)
 
     try:
         ami = ec2.Image(image_id)
@@ -103,44 +111,45 @@ def promote_ami(ctx,
     exitcode = 0
     promoted_tag_found = False
     for tag in ami.tags:
-        if tag['Key'] == 'Promoted':
+        if tag["Key"] == "Promoted":
             promoted_tag_found = True
-            if tag['Value'] == '1':
-                exit_invoke(1, 'The {} AMI is already promoted', image_id)
-            tag['Value'] = '1'
-        if tag['Key'] == 'Name':
-            if not tag['Value'].startswith('PROMOTED //'):
-                tag['Value'] = 'PROMOTED // {}'.format(tag['Value'])
+            if tag["Value"] == "1":
+                exit_invoke(1, "The {} AMI is already promoted", image_id)
+            tag["Value"] = "1"
+        if tag["Key"] == "Name":
+            if not tag["Value"].startswith("PROMOTED //"):
+                tag["Value"] = "PROMOTED // {}".format(tag["Value"])
 
     if promoted_tag_found is False:
-        ami.tags.append({'Key': 'Promoted', 'Value': '1'})
+        ami.tags.append({"Key": "Promoted", "Value": "1"})
 
     if assume_yes is False:
         if terminal is not None:
-            answer = input(terminal.bold(terminal.yellow('Proceed? [N/y]')) + ' ')
+            answer = input(terminal.bold(terminal.yellow("Proceed? [N/y]")) + " ")
         else:
-            answer = input('Proceed? [N/y] ')
-        if not answer or not answer.lower().startswith('y'):
-            exit_invoke(0, 'Not proceeding.')
+            answer = input("Proceed? [N/y] ")
+        if not answer or not answer.lower().startswith("y"):
+            exit_invoke(0, "Not proceeding.")
 
     try:
         ami.create_tags(Tags=ami.tags, DryRun=dry_run)
     except botocore.exceptions.ClientError as exc:
-        if 'DryRunOperation' not in str(exc):
+        if "DryRunOperation" not in str(exc):
             error(exc)
             exitcode = 1
         else:
             warn(exc)
 
     try:
-        if not ami.description.startswith('PROMOTED '):
-            ami.modify_attribute(Attribute='description', Value='PROMOTED ' + ami.description, DryRun=dry_run)
+        if not ami.description.startswith("PROMOTED "):
+            ami.modify_attribute(
+                Attribute="description", Value="PROMOTED " + ami.description, DryRun=dry_run
+            )
     except botocore.exceptions.ClientError as exc:
-        if 'DryRunOperation' not in str(exc):
+        if "DryRunOperation" not in str(exc):
             error(exc)
             exitcode = 1
         else:
             warn(exc)
 
     exit_invoke(exitcode)
-

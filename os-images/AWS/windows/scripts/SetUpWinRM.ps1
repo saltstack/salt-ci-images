a1eca0b2cdd2e81de25100452513ecd956883a5c
Pedro Algarvio <palgarvio@vmware.com>
2022-11-26 19:28:57 +0000

    Increase maximum winrm memory(equal to previous golden images)
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

--- os-images/AWS/windows/scripts/SetUpWinRM.ps1
+++ os-images/AWS/windows/scripts/SetUpWinRM.ps1
@@ -25,7 +25,7 @@ Write-Host "(host) setting up WinRM"
 
 cmd.exe /c winrm quickconfig -q
 cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
-cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
+cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="5000"}'
 cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
 cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
 cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'

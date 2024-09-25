386b5f34d324b5ef93be62ebc6be3f7567f1b17c
Carlos Álvaro <carlos@cdalvaro.io>
2024-09-25 16:51:36 +0200

    feat(ubuntu): Install brew package manager on x86_64

---
+++ os-images/AWS/ubuntu/scripts/install_brew.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ "$(uname -m)" != 'x86_64' ]]; then
+  echo "brew won't be installed for $(uname -m) architecture"
+  exit 0
+fi
+
+echo "====> Installing homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
